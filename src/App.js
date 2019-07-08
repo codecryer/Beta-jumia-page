eba2a81232a8428f6e79f0c7c2f887b601823687
Teezolusegun <teezolusegun@gmail.com>
2019-07-08 22:23:53 +0100

    Before I merge

--- src/App.js
+++ src/App.js
@@ -2,28 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Rowa from "./images/Rowa";
+import Headerbc from './Headerbc'
+import Footerem from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<<<<<<< HEAD
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-=======
-       <Rowa />
->>>>>>> a0d0c9d033b42bc5255a705154eb0745488cfe54
-      </header>
+      
+       <Headerbc />
+       <Footerem />
+      
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+class Footerem extends Component{
+    render(){
+        return(
+            <div className='Footerem'>
+hhhhhhhh
+            </div>
+        )
+    }
+}
+export default Footerem;
