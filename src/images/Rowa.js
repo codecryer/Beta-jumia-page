d63ea197f74e87c26b0d68fd1bc17599aa1fecd9
Teezolusegun <teezolusegun@gmail.com>
2019-07-08 16:28:16 +0100

    new

---
+++ src/images/Rowa.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+
+
+class Rowa extends Component {
+    render(){
+        return (
+           <div>kkkkkkk</div> 
+                  )
+    }
+}
+
+export default Rowa
