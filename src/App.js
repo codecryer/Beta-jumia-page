9645e73547b0c35e799d6637e698f9514b25e9ba
codecryer <samuel.nzekwe2050@gmail.com>
2019-07-09 19:55:51 +0100

    this has a sign up page at begining level

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <Headerbc /> */}
-        <Signup />
+        <Headerbc />
+        {/* <Signup /> */}
       </div>
     );
   }
--- src/Signup.js
+++ src/Signup.js
@@ -1,17 +1,82 @@
 import React, { Component } from "react";
 import "./App.css";
+import logo3 from "./images/jumialogo.svg";
+import foodimage from "./images/foodicon.png";
+import travelimage from "./images/travelo.png";
+import flightimage from "./images/travel.jpeg";
+import dealimage from "./images/dealsimage.jpeg";
+import houseimage from "./images/houseimage.png";
+import oneimage from "./images/oneimage.png";
+import partyimage from "./images/partyimage.png";
 
 class Signup extends Component {
   render() {
     return (
       <div id="jumia">
-        <div className="first second three four fifth sixth seven" />
-        <div className="down left right">
-          <a href="#" target="_blank" className="lira">
-            <svg viewBox="0 0 24 24" width="16" height="16" />
-            "Sell on Jumia"
-          </a>
+        <div className="jumia1">
+          <div className="jumia2">
+            <a href="#" className="marketplace" target="_blank">
+              Sell on Jumia
+            </a>
+          </div>
+          <div className="jumia3">
+            <div classname="lists" />
+            <ul>
+              <li>
+                <img src={logo3} width="60px" height="13vh" />
+              </li>
+              <li>
+                <a href="#">
+                  <img src={foodimage} width="15px" height="10vh" />
+                  FOOD
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={travelimage} width="15px" height="11vh" />
+                  TRAVEL
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={flightimage} width="15px" height="11vh" />
+                  FLIGHT
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={dealimage} width="15px" height="11vh" />
+                  DEALS
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={houseimage} width="15px" height="11vh" />
+                  HOUSE
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={partyimage} width="15px" height="11vh" />
+                  PARTY
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={oneimage} width="15px" height="11vh" />
+                  ONE
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
+        <header className="header">
+          <section className="wrapper">
+            <div className="logo">
+              <img src={logo3} />
+            </div>
+          </section>
+        </header>
       </div>
     );
   }
