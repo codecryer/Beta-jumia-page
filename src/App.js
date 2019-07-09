91992911f66d7d51c915d254d0f7847fe896a21e
Teezolusegun <teezolusegun@gmail.com>
2019-07-09 23:10:40 +0100

    After sorting things with Samuel

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, {Component} from 'react'
-import Headerbc from './Headerbc'
+import React, { Component } from "react";
+import Headerbc from "./Headerbc";
+import Login from "./slogin";
+import Register from "./Register";
+import Profile from "./Profile";
 
-import './App.css'
-import Footerem from './Footerem';
-import Rowa from './Rowa';
-import Subheaderbc from './Subheaderbc'
+import "./App.css";
+import Footerem from "./Footerem";
+import Rowa from "./Rowa";
+import Subheaderbc from "./Subheaderbc";
+import { Link, Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import Home from "./Home";
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <div className="main">
-        <Headerbc/>
-        <Subheaderbc/>
-        <Rowa/>
-        <Footerem />
-      </div>
-    )
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/Home" component={Home} />
+          <Route path="/Profile" component={Profile} />
+          <Route path="/Register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Subheaderbc from "./Subheaderbc";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <Subheaderbc />
+      </div>
+    );
+  }
+}
+export default Register;
