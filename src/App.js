644ea5b5974373005720b213e478d7b6ae63802d
Barnabas <Barnabas-chimaobi>
2019-07-09 15:03:56 +0100

    header with footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import Headerbc from './Headerbc'
-
 import './App.css'
 import Footerem from './Footerem';
 
--- src/Subheaderbc.js
+++ src/Subheaderbc.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import basket from './image/jimage4.png'
+import { NavLink, BrowserRouter} from 'react-router-dom'
 
 class Subheaderbc extends Component {
   render () {
     return (
+      <BrowserRouter>
       <div className='subcontainerbc'>
         <div className='containbc'>
           <div className='jumiabasketbc'>
@@ -16,9 +18,10 @@ class Subheaderbc extends Component {
         </div>
         <div className='searchbc'>
           <input type='text' placeholder='Search for products, brands and categories'/>
-          <button>SEARCH</button>
+          <button>SEARCH</button><NavLink></NavLink>
         </div>
       </div>
+      </BrowserRouter>
     ) 
   }
 }
