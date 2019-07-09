86641752ec7509c4cc66ac5ffdaf2eb50755da1d
Barnabas <Barnabas-chimaobi>
2019-07-09 05:46:16 +0100

    subheader addeded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import Headerbc from './Headerbc'
+import Subheaderbc from './Subheaderbc'
 class App extends Component {
   render () {
     return (
       <div>
         <Headerbc/>
+        <Subheaderbc/>
       </div>
     )
   }
